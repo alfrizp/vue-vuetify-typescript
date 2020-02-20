fe48fd31830558d88420d986b1de50f3ad5adebb
alfrizp <alifrizkipambudi@gmail.com>
2020-02-20 11:51:12 +0700

    news list at top stories page

---
+++ src/services/NewsService.ts
@@ -0,0 +1,25 @@
+import { NewsArticle, ArticleType } from '../types';
+
+class NewsService {
+  getArticlesByType(articleType: ArticleType): Promise<NewsArticle[]> {
+    return fetch('/data/articles.json')
+      .then((response) => response.json())
+      .then((articles) => {
+        const newsArticles = articles
+          .filter((article: any) => article.articleType === articleType)
+          .map((article: any) => ({
+            id: article.id,
+            title: article.title,
+            content: article.content,
+            dateString: article.dateString,
+            baseImageName: article.baseImageName,
+            articleType: article.articleType,
+            isFavourite: article.isFavourite,
+          } as NewsArticle));
+
+        return newsArticles;
+      });
+  }
+}
+
+export default new NewsService();
